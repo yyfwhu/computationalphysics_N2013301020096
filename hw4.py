461664c46adf27ca198b54dd74f8f24d375850a8
whuyyf <471475484@qq.com>
2016-03-23 16:39:18 +0800

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,16 +1,20 @@
+#import pylab
 from pylab import *
+#define the empty list
 v = []
 t = []
 dt = 0.1
 v.append(0.)
 t.append(0)
 et = 10
+#do the "for" circle
 for i in range(int(et/dt)):
 	vn = v[i] + (10-v[i]) * dt
 	v.append(vn)
 	t.append(dt*(i+1))
 	print v[i]
 
+#plot the picture
 plot(v, color='blue', linewidth = 2.0,linestyle='-')
 
 show()	
